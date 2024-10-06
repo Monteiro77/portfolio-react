2facca54e7a4e3ed08cb051e56468312dd366b3c
Monteiro77 <monteiro240905@gmail.com>
2024-10-06 20:02:32 -0300

    finalizado

--- src/pages/Contato.jsx
+++ src/pages/Contato.jsx
@@ -55,7 +55,7 @@ const Contact = () => {
           className="w-full bg-gray-800 text-white p-3 rounded-lg focus:outline-none"
           rows="5"
         ></textarea>
-        <button
+        <button 
           type="submit"
           className="w-full bg-blue-500 text-white py-3 rounded-lg hover:bg-blue-600 transition duration-300"
         >
